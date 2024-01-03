1e92f7ff9abf4360052cf4cbe77cd2193cd62e51
Vivekkr0605 <vivekkumar45622@gmail.com>
2024-01-03 01:24:51 -0800

    mern project

---
+++ server/controller/auth-controller.js
@@ -0,0 +1,79 @@
+const User = require("../models/user-model");
+
+// *----------------------
+//* Controllers
+// *----------------------
+
+//? In an Express.js application, a "controller" refers to a part of your code that is responsible for handling the application's logic. Controllers are typically used to process incoming requests, interact with models (data sources), and send responses back to clients. They help organize your application by separating concerns and following the MVC (Model-View-Controller) design pattern.
+
+// *-------------------
+// Home Logic
+// *-------------------
+const home = async (req, res) => {
+    try {
+      res.status(200).json({ msg: "Welcome to our home page" });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // *-------------------
+// Registration Logic
+// *-------------------
+const register = async (req, res) => {
+    try {
+        // const data = req.body;
+        console.log(req.body);
+        // res.status(201).json({ message: "User registered successfully" });
+        // res.status(200).json({ msg: data });
+        const { username, email, phone, password } = req.body;
+
+    const userExist = await User.findOne({ email: email });
+
+    if (userExist) {
+      return res.status(400).json({ msg: "email already exists" });
+    }
+
+    const userCreated = await User.create({ username, email, phone, password });
+
+    // res.status(201).json({ message: "User registered successfully" });
+    res.status(201).json({ msg: "registration successful", token : await userCreated.generateToken(), userId: userCreated._id.toString(),
+    });
+      } catch (error) {
+        res.status(500).json({ message: "Internal server error" });
+      }
+    };
+
+
+    // *-------------------------------
+//* User Login Logic 📝
+// *-------------------------------
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const userExist = await User.findOne({ email });
+
+    if (!userExist) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    // const user = await bcrypt.compare(password, userExist.password);
+    const isPasswordValid = await userExist.comparePassword(password);
+
+    if (isPasswordValid) {
+      res.status(200).json({
+        message: "Login Successful",
+        token: await userExist.generateToken(),
+        userId: userExist._id.toString(),
+      });
+    } else {
+      res.status(401).json({ message: "Invalid email or passord " });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+  
+  module.exports = { home, register, login };
